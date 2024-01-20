bf7b696cb0c00a2d7a98617441619156d191f43b
Jonatan Arnberg <jonte.arnberg@gmail.com>
2024-01-20 21:48:12 +0100

    installed ffmpeg

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
   const mediaRecorder = useRef(null);
   const [audioChunks, setAudioChunks] = useState([]);
   const [isRecording, setIsRecording] = useState(false);
-  const [allReceivedNotes, setAllReceivedNotes] = useState(['C', 'E', 'G']);
+  const [allReceivedNotes, setAllReceivedNotes] = useState([['C', 'E', 'G']]);
   const [chord, setChord] = useState("Welcome");
   const [progression, setProgression] = useState([]);
   const [audioUrl, setAudioUrl] = useState(); // URL of the audio file
